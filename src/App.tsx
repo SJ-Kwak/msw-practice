000a018cffc71edb1740c46669c774f5edc20c80
SJ-Kwak <shannon.kwak@chequer.io>
2024-07-12 18:42:37 +0900

    axios 통신 모킹

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+interface ResType {
+  id: string;
+  name: string;
+  country: string;
+  lang: string;
+}
 
 function App() {
+  const [peopleData, setPeopleData] = useState<ResType[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.get("/people");
+      console.log(res.data);
+      if (res.status === 200) {
+        setPeopleData(res.data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {peopleData.length > 0 ? (
+        peopleData.map((item: ResType) => (
+          <div key={item.id}>
+            <p>이름: {item.name}</p>
+            <p>나라: {item.country}</p>
+            <p>언어: {item.lang}</p>
+          </div>
+        ))
+      ) : (
+        <p>...로딩 중</p>
+      )}
     </div>
   );
 }
